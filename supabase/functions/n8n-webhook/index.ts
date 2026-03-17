39b47de5fb59bab2d5d01647464f6ef2f42cdd11
WillamsKennedy <willamsknd@gmail.com>
2026-03-17 12:54:47 -0300

    Update index.ts

--- supabase/functions/n8n-webhook/index.ts
+++ supabase/functions/n8n-webhook/index.ts
@@ -5,7 +5,7 @@ const corsHeaders = {
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-supabase-client-platform, x-supabase-client-platform-version, x-supabase-client-runtime, x-supabase-client-runtime-version',
 };
 
-const N8N_BASE_URL = 'https://willamsknd.app.n8n.cloud/webhook';
+const N8N_BASE_URL = 'https://willamsknd.app.n8n.cloud/webhook-teste';
 
 serve(async (req) => {
   if (req.method === 'OPTIONS') {
